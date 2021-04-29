42f0705148c1ebdfc86de0ee18a82e1e2a555e1b
krptstc <krptstc@seznam.cz>
2021-04-29 20:25:55 +0200

    Red Color

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ DECK_HEIGHT = 100
 DECK_COLOR  = (40, 40, 40)
 DECK        = pygame.Rect(0, SCREEN_HEIGHT - DECK_HEIGHT, SCREEN_WIDTH, DECK_HEIGHT)
 
+COLOR_RED   = (200, 0, 0)
+
+BOX_WIDTH   = 65
+BOX_HEIGHT  = 30
+
+RED_BOX     = pygame.Rect(20, SCREEN_HEIGHT - DECK_HEIGHT + 20, BOX_WIDTH, BOX_HEIGHT)
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -18,5 +25,6 @@ while running:
     SCREEN.fill((0, 0, 0))
 
     pygame.draw.rect(SCREEN, DECK_COLOR, DECK)
+    pygame.draw.rect(SCREEN, COLOR_RED, RED_BOX)
 
     pygame.display.update()
