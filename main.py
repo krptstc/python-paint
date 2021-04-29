a102951d6bd5d67aa74c151451825358badf90c1
krptstc <krptstc@seznam.cz>
2021-04-29 19:57:48 +0200

    Deck Base

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ SCREEN_HEIGHT = 400
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Paint')
 
+DECK_HEIGHT = 100
+DECK_COLOR  = (40, 40, 40)
+DECK        = pygame.Rect(0, SCREEN_HEIGHT - DECK_HEIGHT, SCREEN_WIDTH, DECK_HEIGHT)
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -12,4 +16,7 @@ while running:
             running = False
 
     SCREEN.fill((0, 0, 0))
+
+    pygame.draw.rect(SCREEN, DECK_COLOR, DECK)
+
     pygame.display.update()
